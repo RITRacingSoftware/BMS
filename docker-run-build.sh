d6d42976cf97f3f278385d52bfe0e38513e22c82
chrisblust <chblust98@gmail.com>
2020-09-13 17:48:11 -0400

    added script for building docker image, removed container removal line in other docker scripts

--- docker-run-build.sh
+++ docker-run-build.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-docker rm f29bms
 docker run --name f29bms -v $PWD:/f29bms f29bms './build.sh'
--- enter-docker.sh
+++ enter-docker.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-docker rm f29bms
 docker run -it -v $PWD:/f29bms f29bms
