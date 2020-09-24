e1524002c62e08c515f87d02fbac733d2516eae3
chrisblust <chblust98@gmail.com>
2020-09-24 13:55:40 -0400

    updated dummy CurrentSense implementation to desired implementation with tests

--- src/app/CurrentSense/CurrentSense.c
+++ src/app/CurrentSense/CurrentSense.c
@@ -1,20 +1,74 @@
 #include "CurrentSense.h"
 #include "HAL_CurrentSensor.h"
 #include "FaultManager.h"
+#include <stdio.h>
 
-float CurrentSense_get_current(void)
+// kept track of in order to not filter on garbage memory
+static bool first_reading_received;
+
+// kept track of to prevent application code from using unfiltered current
+static bool filter_valid;
+
+// low pass filter history (only one value for now)
+static float last_current_reading;
+
+static bool did_last_read_error;
+
+void filter_new_current_reading(float new_reading_A)
 {
-    float latest_current; 
-    Error_t err = HAL_CurrentSensor_read_current(&latest_current);
+    if (first_reading_received)
+    {
+        // dummy simple low pass filter
+        // y[n] = (x[n] + x[n-1]) * CURRENT_SENSE_LPF_COEFF
+        last_current_reading = (last_current_reading + new_reading_A) * CURRENT_SENSE_LPF_COEFF;
 
+        // data in last_current_reading is now filtered, safe to use
+        filter_valid = true;
+    }
+    else
+    {
+        last_current_reading = new_reading_A;
+        first_reading_received = true;
+    }
+}
+
+void CurrentSense_init(void)
+{
+    first_reading_received = false;
+    filter_valid = false;
+    did_last_read_error = false;
+}
+
+void CurrentSense_1kHz(void)
+{
+    // Get new current reading from ADC
+    float raw_current_A;
+    Error_t err = HAL_CurrentSensor_read_current(&raw_current_A);
+
+    // Verify no errors accessing ADC
     if (err.active)
     {
+        did_last_read_error = true;
         FaultManager_set_fault_active(FaultCode_CURRENT_SENSOR_COMM, err.data, false);
     }
     else
     {
+        did_last_read_error = false;
         FaultManager_clear_fault(FaultCode_CURRENT_SENSOR_COMM);
+        filter_new_current_reading(raw_current_A);
     }
+}
 
-    return latest_current;
+bool CurrentSense_get_current(float* current_A)
+{
+    // save this off
+    // keeps actions from here till return value consistent in case task is preempted...
+    bool inst_current_valid = filter_valid && !did_last_read_error;
+
+    if (inst_current_valid)
+    {
+        *current_A = last_current_reading;
+    }
+    
+    return inst_current_valid;
 }
--- src/app/CurrentSense/test_CurrentSense.c
+++ src/app/CurrentSense/test_CurrentSense.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 // includes for mocking
 #include "unity.h"
 
@@ -8,30 +10,151 @@
 #include "MockHAL_CurrentSensor.h"
 #include "MockFaultManager.h"
 
-/*
- * Purpose: verify current reading from sensor is forwarded to application layer.
- * I.e., current sensor reading should be equal to value returned from CurrentSense_get_current.
- * Additionally, verify fault is not set if no fault is indicated by sensor.
+// arbitrary cap on the time it takes for the filter to initialize
+// unit tests will fail if it takes longer
+#define MAX_FILTER_INIT_MS 20
+
+/**
+ * Verify that the CurrentSense module is in fact using a low pass filter on the sensor readings.
  */
-void test_CurrentSense_get_current(void)
+void test_CurrentSense_low_pass_filter(void)
 {
-    // injected current that should be returned
-    float TEST_CURRENT = 1.2345;
-    // injected fault that should be returned
+    // here instead of in setUp
+    // this is because this function uses resetTest, which avoids memory
+    // issues but also calls setUp and we dont want init running each for loop iteration.
+    CurrentSense_init();
+
+    // this test keeps current at CURRENT_STEADY
+    // and periodically inputs CURRENT_SPIKE for a sample period
+    const float CURRENT_STEADY = 10.0;
+    const float CURRENT_SPIKE = 100;
+
+    for (int i = 0; i < 500; i++)
+    {
+        float current_A;
+
+        // simulate spikes of current
+        if (i % 10)
+        {
+            current_A = 10.0;
+        }
+        else
+        {
+            current_A = 100.0;
+        }
+
+        Error_t err;
+        err.active = false;
+
+        // inject test current and nominal error status
+        HAL_CurrentSensor_read_current_ExpectAnyArgsAndReturn(err);
+        HAL_CurrentSensor_read_current_ReturnThruPtr_current(&current_A);
+        // no active errors so fault clear should happen
+        FaultManager_clear_fault_ExpectAnyArgs();
+
+        CurrentSense_1kHz();
+        
+        float read_current_A;
+        bool current_valid = CurrentSense_get_current(&read_current_A);
+
+        // arbitrary indicator that the current is taking too long to go valid
+        if ((i >= MAX_FILTER_INIT_MS) && !current_valid)
+        {
+            TEST_ASSERT_MESSAGE(false, "Filters failed to initialize.");
+        }
+
+        if (current_valid)
+        {
+            // verify current is always less than the spike current
+            // and >= the steady current
+            char err_msg[100];
+            sprintf(err_msg, "Current (%fA) outside interval: [%f,%f)", read_current_A, CURRENT_STEADY, CURRENT_SPIKE);
+
+            TEST_ASSERT_MESSAGE(read_current_A >= CURRENT_STEADY, err_msg);
+            TEST_ASSERT_MESSAGE(read_current_A < CURRENT_SPIKE, err_msg);
+        }
+
+        // Tells CMock to evaluate expectations here then clean up memory.
+        // Otherwise CMock waits till every for loop iteration is done and blows up in memory.
+        resetTest();
+    }
+}
+
+/**
+ * Verify a fault is triggered if the current sensor returns an error.
+ * Additionally, check that the current is always indicated as invalid if an error occurred in the last read.
+ */
+void test_CurrentSense_sensor_fault(void)
+{
+    CurrentSense_init();
+
     Error_t err;
     err.active = false;
 
-    // should clear faults since no error returned
-    FaultManager_clear_fault_ExpectAnyArgs();
+    // give the filter plenty of time to intialize
+    for (int i = 0; i < MAX_FILTER_INIT_MS; i++)
+    {
+        float current_A = 10;
 
-    // mock a nominal response from the current sensor
+        HAL_CurrentSensor_read_current_ExpectAnyArgsAndReturn(err);
+        HAL_CurrentSensor_read_current_ReturnThruPtr_current(&current_A);
+        // no active errors so fault clear should happen
+        FaultManager_clear_fault_ExpectAnyArgs();
+
+        CurrentSense_1kHz();
+    }
+
+    // verify filters initialized
+    float read_current_A;
+    TEST_ASSERT_MESSAGE(CurrentSense_get_current(&read_current_A) == true, "Filters failed to initialize.");
+
+    // now fault current
+    err.active = true;
+
+    // should trigger fault
     HAL_CurrentSensor_read_current_ExpectAnyArgsAndReturn(err);
-    // mock the test current to be returned through the out parameter
-    HAL_CurrentSensor_read_current_ReturnThruPtr_current(&TEST_CURRENT);
+    FaultManager_set_fault_active_ExpectAnyArgs();
+    CurrentSense_1kHz();
+    TEST_ASSERT_MESSAGE(CurrentSense_get_current(&read_current_A) == false, "Sensor error but CurrentSense indicated valid.");
+}
 
-    // this test will fail if a fault is set since FaultManager_set_fault_active is not expected
-    
-    // verify the current reading is what we injected
-    float current = CurrentSense_get_current();
-    TEST_ASSERT(current == TEST_CURRENT);
+/**
+ * Verify that when the current sensor reads a steady current, the CurrentSense
+ * module returns the steady value. In other words, check for filter errors that 
+ * erroneously alter a steady state current.
+ */
+void test_CurrentSense_steady(void)
+{
+    CurrentSense_init();
+
+    for(int i = 0; i < 100; i++)
+    {
+        float injection_current_A = 7;
+        Error_t err;
+        err.active = false;
+        // inject test current and nominal error status
+        HAL_CurrentSensor_read_current_ExpectAnyArgsAndReturn(err);
+        HAL_CurrentSensor_read_current_ReturnThruPtr_current(&injection_current_A);
+        // no active errors so fault clear should happen
+        FaultManager_clear_fault_ExpectAnyArgs();
+
+        CurrentSense_1kHz();
+        
+        float read_current_A;
+        bool current_valid = CurrentSense_get_current(&read_current_A);
+
+        if ((i >= MAX_FILTER_INIT_MS) && !current_valid)
+        {
+            TEST_ASSERT_MESSAGE(false, "Filters failed to initialize.");
+        }
+
+        if (current_valid)
+        {
+            char err_msg[100];
+            sprintf(err_msg, "Current read (%fA) on iteration %d did not match injected steady state current (%fA).", read_current_A, i, injection_current_A);
+            TEST_ASSERT_MESSAGE(read_current_A == injection_current_A, err_msg);
+        }
+
+        resetTest();
+    }
 }
