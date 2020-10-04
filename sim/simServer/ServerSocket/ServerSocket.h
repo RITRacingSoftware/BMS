5e42d89c9d7cddb116c04209c49c7997f7de42ac
Paul Kelly <pjk2563@rit.edu>
2020-10-03 21:43:10 -0400

    Stubbed out SocketServer code

--- sim/simServer/ServerSocket/ServerSocket.h
+++ sim/simServer/ServerSocket/ServerSocket.h
@@ -2,6 +2,7 @@
 #define SERVER_SOCKET_H
 
 // start the socket server 
+// look up how to init a pthread function
 void socket_server(void *fd); // blocking; should be run in thread
 
 // kill the socket server
