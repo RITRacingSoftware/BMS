c8b14e813c8f4f4fcf6ebbbcd261485a5a97c3ff
CRP5488 <crp5488@ip-172-31-21-82.us-east-2.compute.internal>
2021-02-06 10:58:40 -0500

    Added simple test

--- src/driver/HAL_Gpio/test_STM32_HAL_Gpio.c
+++ src/driver/HAL_Gpio/test_STM32_HAL_Gpio.c
@@ -1,4 +1,12 @@
+#include HAL_Gpio.h
+
 int main()
 {
     // TODO- implement
+    HAL_GPIO_Init();
+    bool chargeAvailableState = HAL_Gpio_read(CHARGE_AVAILABLE_PIN);
+    printf("Charge available state: %d", chargeAvailableState);
+    HAL_Gpio_write(STATUS_LED_PIN, true);
+    HAL_Gpio_write(CHARGE_ENABLE_PIN, true);
+
 }
