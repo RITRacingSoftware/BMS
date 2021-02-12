c4e50aa1f571ec2453f40fe2746a6b2a1a70c185
CRP5488 <crp5488@rit.edu>
2021-02-11 21:40:59 -0500

    Changed to SPI2

--- src/driver/HAL_IsoSpi/STM32_HAL_IsoSpi.c
+++ src/driver/HAL_IsoSpi/STM32_HAL_IsoSpi.c
@@ -4,21 +4,21 @@
 #include "stm32f0xx.h"
 #include "stm32f0xx_gpio.h"
 
-#define ISOSPI_PINS 0xC0
+#define ISOSPI_PINS 0xF00
 
 void HAL_IsoSpi_Init()
 {
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
-    GPIO_PinAFConfig(GPIOA, ISOSPI_PINS, GPIO_Mode_AF);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE); //Using SPI 2
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+    GPIO_PinAFConfig(GPIOB, ISOSPI_PINS, GPIO_Mode_AF);
     GPIO_InitTypeDef isospiGPIOinit = {ISOSPI_PINS, GPIO_Mode_AF, GPIO_Speed_Level_3, GPIO_OType_PP, GPIO_PuPd_NOPULL}; //Not sure about speed, OType, and PuPd
-    GPIO_Init(GPIOA, &isospiGPIOinit);
+    GPIO_Init(GPIOB, &isospiGPIOinit);
     SPI_InitTypeDef spiInit;
     SPI_StructInit(&spiInit); //Fills in default values
     //Need to change any not default settings
-    SPI_Init(SPI1, &spiInit);
-    SPI_RxFIFOThresholdConfig(SPI1, SPI_RxFIFOThreshold_HF); //Don't know if should be HF or QF
-    SPI_Cmd(SPI1, ENABLE);
+    SPI_Init(SPI2, &spiInit);
+    SPI_RxFIFOThresholdConfig(SPI2, SPI_RxFIFOThreshold_HF); //Don't know if should be HF or QF
+    SPI_Cmd(SPI2, ENABLE);
 
 }
 
@@ -28,22 +28,22 @@ Error_t HAL_IsoSpi_transmit_and_receive(PIN slave_select, char* tx, int tx_len, 
     error.active = false;
     for(int i = 0; i < tx_len; i++) //transmit data
     {
-        if(SPI_GetTransmissionFIFOStatus(SPI1) == SPI_TransmissionFIFOStatus_Full)
+        if(SPI_GetTransmissionFIFOStatus(SPI2) == SPI_TransmissionFIFOStatus_Full)
         {
             error.active = true;
             return error;
         }
-        SPI_SendData8(SPI1, tx[i]);
+        SPI_SendData8(SPI2, tx[i]);
         
     }
     for(int i = 0; i < rx_len; i++) //recieve data
     {
-        if(SPI_GetReceptionFIFOStatus(SPI1) == (SPI_ReceptionFIFOStatus_Empty || SPI_ReceptionFIFOStatus_Full)) //Nor sure if right
+        if(SPI_GetReceptionFIFOStatus(SPI2) == (SPI_ReceptionFIFOStatus_Empty || SPI_ReceptionFIFOStatus_Full)) //Nor sure if right
         {
             error.active = true;
             return error;
         }
-        rx[i] = SPI_ReceiveData8(SPI1);
+        rx[i] = SPI_ReceiveData8(SPI2);
     }
     return error;
 
