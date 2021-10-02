011edd06a7bfbba341210b8377db8c9295099044
CRP5488 <crp5488@rit.edu>
2021-10-02 11:28:50 -0400

    Changed current limit name in dbc

--- src/app/CurrentLimiter/test_CurrentLimiter.c
+++ src/app/CurrentLimiter/test_CurrentLimiter.c
@@ -21,7 +21,7 @@ void test_CurrentLimiter_selectMinCurrent(void)
     BatteryModel_t bm;
     bm.smallest_V = CHARGED_CELL_V; //Shouldn't really matter, only used to pass to calculation and correction functions 
 
-    can_bus.bms_current_limit.max_discharge_current = f29bms_dbc_current_limit_max_discharge_current_encode(MIN_ALLOWED_CURRENT_LIMIT+10);
+    can_bus.bms_current_limit.bms_max_discharge_current = f29bms_dbc_bms_current_limit_bms_max_discharge_current_encode(MIN_ALLOWED_CURRENT_LIMIT+10);
     
     
     
@@ -35,8 +35,8 @@ void test_CurrentLimiter_selectMinCurrent(void)
     CurrentSense_get_current_ExpectAnyArgsAndReturn(true);
     CurrentLimiter_10Hz(&bm);
     char err_msg[150];
-    sprintf(err_msg, "Current limit set above maximum allowed current limit.(%fA)", f29bms_dbc_current_limit_max_discharge_current_decode(can_bus.bms_current_limit.max_discharge_current));
-    TEST_ASSERT_MESSAGE(f29bms_dbc_current_limit_max_discharge_current_decode(can_bus.bms_current_limit.max_discharge_current) == MAX_ALLOWED_CURRENT_LIMIT, err_msg);
+    sprintf(err_msg, "Current limit set above maximum allowed current limit.(%fA)", f29bms_dbc_bms_current_limit_bms_max_discharge_current_decode(can_bus.bms_current_limit.bms_max_discharge_current));
+    TEST_ASSERT_MESSAGE(f29bms_dbc_bms_current_limit_bms_max_discharge_current_decode(can_bus.bms_current_limit.bms_max_discharge_current) == MAX_ALLOWED_CURRENT_LIMIT, err_msg);
     
     
     current_correction = MIN_ALLOWED_CURRENT_LIMIT + 100;
@@ -50,7 +50,7 @@ void test_CurrentLimiter_selectMinCurrent(void)
 
     //Make sure current limit set to the calculated current
     CurrentLimiter_10Hz(&bm);
-    TEST_ASSERT_MESSAGE(f29bms_dbc_current_limit_max_discharge_current_decode(can_bus.bms_current_limit.max_discharge_current) == MIN_ALLOWED_CURRENT_LIMIT + 50, "Current limit not set to lower limit value (calculated).");
+    TEST_ASSERT_MESSAGE(f29bms_dbc_bms_current_limit_bms_max_discharge_current_decode(can_bus.bms_current_limit.bms_max_discharge_current) == MIN_ALLOWED_CURRENT_LIMIT + 50, "Current limit not set to lower limit value (calculated).");
 
     //Change correction to lowet
     current_correction = MIN_ALLOWED_CURRENT_LIMIT + 25;
@@ -62,7 +62,7 @@ void test_CurrentLimiter_selectMinCurrent(void)
 
     //Make sure current limit set to the corrected current
     CurrentLimiter_10Hz(&bm);
-    TEST_ASSERT_MESSAGE(f29bms_dbc_current_limit_max_discharge_current_decode(can_bus.bms_current_limit.max_discharge_current) == MIN_ALLOWED_CURRENT_LIMIT + 25, "Current limit not set to lower limit value (correction).");
+    TEST_ASSERT_MESSAGE(f29bms_dbc_bms_current_limit_bms_max_discharge_current_decode(can_bus.bms_current_limit.bms_max_discharge_current) == MIN_ALLOWED_CURRENT_LIMIT + 25, "Current limit not set to lower limit value (correction).");
     
     //Make sure limit doesn't go below the minimum allowed current limit
     current_calculated = MIN_ALLOWED_CURRENT_LIMIT - 5;
@@ -72,5 +72,5 @@ void test_CurrentLimiter_selectMinCurrent(void)
     CurrentLimitCorrection_getCorrection_ReturnThruPtr_currentLimit(&current_correction);
     CurrentSense_get_current_ExpectAnyArgsAndReturn(true);
     CurrentLimiter_10Hz(&bm);
-    TEST_ASSERT_MESSAGE(f29bms_dbc_current_limit_max_discharge_current_decode(can_bus.bms_current_limit.max_discharge_current) == MIN_ALLOWED_CURRENT_LIMIT, "Current limit set below minimum allowed current limit.");
+    TEST_ASSERT_MESSAGE(f29bms_dbc_bms_current_limit_bms_max_discharge_current_decode(can_bus.bms_current_limit.bms_max_discharge_current) == MIN_ALLOWED_CURRENT_LIMIT, "Current limit set below minimum allowed current limit.");
 }
