89afd30442fee8278784bcfef7c0e96170e2cc7c
Akari Neukirch <mnn1262@rit.edu>
2024-04-02 23:30:00 -0400

    F32 4x19 poop loop

--- src/app/CellBalancer/CellBalancer.c
+++ src/app/CellBalancer/CellBalancer.c
@@ -18,63 +18,20 @@ void CellBalancer_init(void)
     }
 }
 
+static long bstimer;
+
 void CellBalancer_stage_cell_draining(BatteryModel_t* bm)
 {
     // keep track of per iteration drain failure detection
     bool drain_failure = false;
 
+    bstimer++;
+
     for (int i = 0; i < NUM_SERIES_CELLS; i++)
     {
-        // check if this cell should be draining and isnt
-        if (bm->cells[i].is_draining != prev_model.cells[i].is_draining)
-        {
-            FaultManager_set_fault_active(FaultCode_DRAIN_FAILURE, &i);
-            drain_failure = true;
-        }
-        
-        if (ChargeMonitor_is_balancing_allowed())
-        {
-            float cell_diff_V = bm->cells[i].voltage - bm->smallest_V;
-          // bm->cells[i].is_draining = true; 
-            //start some cells draining
-            if (FLOAT_GT(cell_diff_V, DIFF_CORRECTION_THRESHOLD_V, VOLTAGE_TOLERANCE))
-            {
-                // cell is falling behind, request drain
-                bm->cells[i].is_draining = true;
-            }
-            else if (FLOAT_GT_EQ(bm->cells[i].voltage, CHARGED_CELL_V, VOLTAGE_TOLERANCE))
-            {
-                // charging is supposed to happen, but this cell needs to be drained first
-                bm->cells[i].is_draining = true;
-            }
-            else
-            {
-                // stop some cells draining
-                if (bm->cells[i].is_draining)
-                {
-                    if (FLOAT_LT_EQ(bm->cells[i].voltage, CHARGED_CELL_V - BALANCING_HISTERESIS_V, VOLTAGE_TOLERANCE))
-                    {
-                        bm->cells[i].is_draining = false;
-                    }
-                }
-                else
-                {
-                    bm->cells[i].is_draining = false;      
-                }
-            }
-            //bm->cells[i].is_draining = true;
-            reading_allowed = false;
-        }
-        else
-        {
-            bm->cells[i].is_draining = false;
-            reading_allowed = true;
-        }
-    }
-
-    if (drain_failure == false)
-    {
-        FaultManager_clear_fault(FaultCode_DRAIN_FAILURE);
+        int maxcell = ((bstimer/2) % (NUM_SERIES_CELLS+10));
+        bm->cells[i].is_draining = (i < maxcell);
+        //bm->cells[i].is_draining = (((bstimer / 50) % 2) == 0) && (i < 3);
     }
 
     // save off the new expected drain states
