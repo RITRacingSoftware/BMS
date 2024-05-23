b4143e760d02b53e1785241fbceaa109e4f37b46
Akari Neukirch <mnn1262@rit.edu>
2024-05-23 12:50:48 -0700

    Removed limp mode defines from config

--- src/f29BmsConfig.h
+++ src/f29BmsConfig.h
@@ -145,37 +145,5 @@ static float temp_lut_V[NUM_TEMP_RANGES][3] =
  */
 #define MAX_POWER_LIMIT_KW 70.0
 
-/**
- * Limp Mode
-*/
-
-// These are the different levels of limp mode, with level 1 being the least limiting, and level 3 being the most limiting
-#define LIMP_MODE_LEVEL_1_CURRENT_A 175.0
-#define LIMP_MODE_LEVEL_2_CURRENT_A 115.0
-#define LIMP_MODE_LEVEL_3_CURRENT_A 95.0
-#define LIMP_MODE_LEVEL_4_CURRENT_A 75.0
-#define LIMP_MODE_LEVEL_5_CURRENT_A 40.0
-
-// These are the voltages when the levels of limp modes will be entered
-#define LIMP_MODE_VOLTAGE_LEVEL_1 3.4
-#define LIMP_MODE_VOLTAGE_LEVEL_2 3.4
-#define LIMP_MODE_VOLTAGE_LEVEL_3 3.4
-#define LIMP_MODE_VOLTAGE_LEVEL_4 3.4 
-#define LIMP_MODE_VOLTAGE_LEVEL_5 3.25
-
-// These are the times that the voltage has to be below the threshold before entering the limp mode
-#define LIMP_MODE_VOLTAGE_LEVEL_1_TIME_MS 10000 //Change back to 2500 for Endurance
-#define LIMP_MODE_VOLTAGE_LEVEL_2_TIME_MS 10000 //Change back to 2500 for Endurance
-#define LIMP_MODE_VOLTAGE_LEVEL_3_TIME_MS 2500 //Change back to 2500 for Endurance
-#define LIMP_MODE_VOLTAGE_LEVEL_4_TIME_MS 0
-#define LIMP_MODE_VOLTAGE_LEVEL_5_TIME_MS 0
-
-
-// These are the temperatures when the levels of limp modes will be entered
-#define LIMP_MODE_TEMP_LEVEL_1 48.0 //53.0
-#define LIMP_MODE_TEMP_LEVEL_2 51.0 //54.0
-#define LIMP_MODE_TEMP_LEVEL_3 53.0 //55.0
-#define LIMP_MODE_TEMP_LEVEL_4 55.0 //56.0
-#define LIMP_MODE_TEMP_LEVEL_5 57.0 //57.0
 
 #endif // F29BMSCONFIG_H
